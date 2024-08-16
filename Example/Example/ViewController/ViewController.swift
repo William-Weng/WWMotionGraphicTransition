9dd98344169dc3de1170e590e9326ec5d45dc899
William.Weng <William.Weng@iit.com.hk>
2024-08-16 17:46:56 +0800

    - update Example

--- Example/Example/ViewController/ViewController.swift
+++ Example/Example/ViewController/ViewController.swift
@@ -14,44 +14,57 @@ final class ViewController: UIViewController {
     @IBOutlet weak var faceImageView: UIImageView!
     
     private let count = 5
-    private let colors: [UIColor] = [.red, .yellow, .green, .blue, .orange]
     private let duration: TimeInterval = 0.5
-    
+    private let colors: [UIColor] = [.red, .yellow, .blue, .green, .orange]
+
     private var doorCurtain: WWMotionGraphicTransition.DoorCurtain!
+    private var fanBlade: WWMotionGraphicTransition.FanBlade!
     
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     @IBAction func doorCurtainEffect(_ sender: UIBarButtonItem) {
-
+        
         doorCurtain = WWMotionGraphicTransition.DoorCurtain.build(frame: faceImageView.bounds)
         doorCurtain.delegate = self
         faceImageView.addSubview(doorCurtain)
         
         doorCurtain.start(duration: duration, count: count, colors: colors)
     }
+    
+    @IBAction func fanBladeEffect(_ sender: UIBarButtonItem) {
+        
+        fanBlade = WWMotionGraphicTransition.FanBlade.build(frame: faceImageView.bounds)
+        fanBlade.delegate = self
+        faceImageView.addSubview(fanBlade)
+        
+        fanBlade.start(count: count, colors: colors)
+    }
 }
 
-// MARK: - WWMotionGraphicTransitionDoorCurtainDelegate
-extension ViewController: WWMotionGraphicTransitionDoorCurtainDelegate {
+// MARK: - WWMotionGraphicTransitionDelegate
+extension ViewController: WWMotionGraphicTransitionDelegate {
     
-    func start(doorCurtain: WWMotionGraphicTransition.DoorCurtain, index: Int, status: WWMotionGraphicTransition.DoorCurtain.Status) {
+    func start(effectView: UIView, number: Int, status: WWMotionGraphicTransition.Status) {
         
         faceImageView.image = UIImage(named: "Face1")
-        
-        if ((index + 1) < count) { return }
+                
+        if (number < count) { return }
         if (status != .end) { return }
         
         faceImageView.image = UIImage(named: "Face2")
-        doorCurtain.end(duration: duration)
+        
+        if effectView is WWMotionGraphicTransition.DoorCurtain { doorCurtain.end(duration: duration); return }
+        if effectView is WWMotionGraphicTransition.FanBlade { fanBlade.end(duration: duration); return }
     }
     
-    func end(doorCurtain: WWMotionGraphicTransition.DoorCurtain, index: Int, status: WWMotionGraphicTransition.DoorCurtain.Status) {
+    func end(effectView: UIView, number: Int, status: WWMotionGraphicTransition.Status) {
         
-        if ((index + 1) < count) { return }
+        if (number < count) { return }
         if (status != .end) { return }
         
-        doorCurtain.removeFromSuperview()
+        if effectView is WWMotionGraphicTransition.DoorCurtain { doorCurtain.removeFromSuperview(); return }
+        if effectView is WWMotionGraphicTransition.FanBlade { fanBlade.removeFromSuperview(); return}
     }
 }
